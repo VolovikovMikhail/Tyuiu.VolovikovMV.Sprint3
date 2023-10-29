be9273ae376ebb4d3b14f53faff6e5deb955cd1f
VolovikovMikhail <volovikov.1995@bk.ru>
2023-10-29 19:47:17 +0500

    Task7

---
+++ Tyuiu.VolovikovMV.Sprint3.Task7.V7.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VolovikovMV.Sprint3.Task7.V7.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VolovikovMV.Sprint3.Task7.V7.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49e78098-f7bc-4db9-85a1-84fa25e3df0d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.VolovikovMV.Sprint3.Task7.V7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VolovikovMV.Sprint3.Task7.V7.Lib;
+
+namespace Tyuiu.VolovikovMV.Sprint3.Task7.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Воловиков М. В. | АСОиУб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант 7                                                               *");
+            Console.WriteLine("* Выполнил: Воловиков Михаил Васильевич | АСОиУб-23-3                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* Напишите программу, которая выводит таблицу значений функции            *");
+            Console.WriteLine("* на заданном диапазоне [-5; 5] с шагом 1.                                *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = -5, stopValue = 5;
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+            double[] valueArray;
+            valueArray = new double[len];
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+            Console.WriteLine("Результат:");
+            Console.WriteLine("+---------+----------+");
+            Console.WriteLine("|    X    |    f(x)  |");
+            Console.WriteLine("+---------+----------+");
+
+            for (int i = 0; i <= len-1; i++)
+            {
+                Console.WriteLine("|{0,5:d}    |  {1,5:f2}   |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+            Console.ReadKey();
+        }
+    }
+}
